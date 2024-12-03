b8e9c92d01a790218a6951626804e331a87a6c94
D4rkWizard <36143732+D4rkWizard@users.noreply.github.com>
2024-12-03 22:24:36 +0700

    Update handler.js

--- src/server/handler.js
+++ src/server/handler.js
@@ -1,3 +1,5 @@
+const storeData = require('../services/storeData');
+
 async function postPredictHandler(request, h) {
     const { image } = request.payload;
     const { model } = request.server.app;
@@ -13,6 +15,8 @@ async function postPredictHandler(request, h) {
       "confidenceScore": confidenceScore,
       "createdAt": createdAt
     }
+
+    await storeData(id, data);
 
     const response = h.response({
         status: 'success',
